66e8cd9cfff56cf4ef1a9c9daf29fcde012b6677
Shivam Singh <shivam152002@gmail.com>
2022-08-05 03:54:36 +0530

    fix: deploy error

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
-import { GetApi } from "./modules/getData.js";
+// import { GetApi } from "./modules/getData.js";
+import fetch from "node-fetch";
 import ejs from "ejs";
 
 const app = express();
@@ -8,10 +9,38 @@ app.set('view engine', 'ejs');
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    let dataItems = GetApi();
-    console.log({dataItems});
+    // let dataItems = GetApi();
+
+    const apiId= 'fbe186b8ecb647f195b195622220408';
+    let dataSet;
+
+        const CityName = "Mumbai";
+        console.log(CityName)
+        const url = `http://api.weatherapi.com/v1/current.json?key=${apiId}&q=${CityName}&aqi=yes`;
+        console.log(url);
+        
+        fetch(url)
+        .then((res) => res.json())
+        .then((data) => {
+            dataSet = {
+                loc: data.location.name,
+                temp: data.current.temp_c,
+            }
+        }).catch((err) => {
+            console.log(err)
+            dataSet = {
+                loc: "Sorry location not found",
+                temp:"Sorry location not found",
+            }
+        })    
+
+
+
+
+    
+    console.log({dataSet});
     res.render("index", {
-        data : dataItems,
+        data : dataSet,
     });
 });
 
